78653428bffff11cbd68b3acaf3397ae7b2006e3
Kanhaiya Thakur <kanthaku1@publicisgroupe.net>
2018-07-05 12:23:42 +0530

    Adding bootstrap demo

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Shorter version
+// $(function(){ alert("hello!"); });
+
+// This function will only execute after document is ready.
+$(document).ready(function() 
+{
+    // alert("hello");
+});
+
